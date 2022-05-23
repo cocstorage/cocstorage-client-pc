bd41b453d26cd6bfe19950d586133c0fbc3a6b8f
kimjh96 <kimjhs@kakao.com>
2022-05-24 00:44:13 +0900

    feat: Index 페이지 prefetchQuery 조정, PWA 활성화

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,11 +13,6 @@ import { IssueKeywordRank, SidePopularStorageList } from '@components/UI/organis
 
 import { fetchIssueKeywordRank } from '@api/v1/issue-keywords';
 import { fetchIndexNotice } from '@api/v1/notices';
-import {
-  fetchIndexPopularStorageBoards,
-  fetchIndexWorstStorageBoards,
-  fetchLatestStorageBoards
-} from '@api/v1/storage-boards';
 
 import queryKeys from '@constants/react-query';
 
@@ -48,20 +43,8 @@ function Index() {
 export async function getServerSideProps() {
   const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery(
-    queryKeys.storageBoards.indexPopularStorageBoards,
-    fetchIndexPopularStorageBoards
-  );
-  await queryClient.prefetchQuery(
-    queryKeys.storageBoards.indexWorstStorageBoards,
-    fetchIndexWorstStorageBoards
-  );
-  await queryClient.prefetchQuery(
-    queryKeys.storageBoards.latestStorageBoards,
-    fetchLatestStorageBoards
-  );
-  await queryClient.prefetchQuery(queryKeys.issueKeywords.issueKeywordRank, fetchIssueKeywordRank);
   await queryClient.prefetchQuery(queryKeys.notices.indexNotice, fetchIndexNotice);
+  await queryClient.prefetchQuery(queryKeys.issueKeywords.issueKeywordRank, fetchIssueKeywordRank);
 
   return {
     props: {
