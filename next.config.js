194a9fd833f8c41e6606725b5aa7572334604c39
kimjh96 <kimjhs@kakao.com>
2022-08-06 23:11:37 +0900

    fix: 일부 이미지 깨짐 문제 수정

--- next.config.js
+++ next.config.js
@@ -16,7 +16,8 @@ module.exports = withPWA({
   env: {
     API_BASE_URL: process.env.API_BASE_URL,
     X_API_KEY: process.env.X_API_KEY,
-    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+    IMAGE_DOMAIN: process.env.IMAGE_DOMAIN
   },
   images: {
     domains: [process.env.IMAGE_DOMAIN]
