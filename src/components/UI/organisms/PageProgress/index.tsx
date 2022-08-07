17684949f5aa574748110786e5f9ae36c399dc1e
kimjh96 <kimjhs@kakao.com>
2022-08-07 19:31:33 +0900

    fix: PageProgress 컴포넌트 로직 재수정

--- src/components/UI/organisms/PageProgress/index.tsx
+++ src/components/UI/organisms/PageProgress/index.tsx
@@ -17,6 +17,7 @@ function PageProgress() {
 
   const progressingTimerRef = useRef<ReturnType<typeof setInterval>>();
   const progressingDoneTimerRef = useRef<ReturnType<typeof setTimeout>>();
+  const valueInitializeTimerRef = useRef<ReturnType<typeof setTimeout>>();
 
   const opacity = useMemo(() => {
     if (isDone) return 0;
@@ -28,9 +29,41 @@ function PageProgress() {
     setIsDone(false);
     setHide(false);
     setValue(0);
+
+    if (progressingTimerRef.current) {
+      clearInterval(progressingTimerRef.current);
+    }
+    if (progressingDoneTimerRef.current) {
+      clearTimeout(progressingDoneTimerRef.current);
+    }
+    if (valueInitializeTimerRef.current) {
+      clearTimeout(valueInitializeTimerRef.current);
+    }
+
+    progressingTimerRef.current = setInterval(() => {
+      setValue((prevValue) => {
+        if (prevValue >= 100) {
+          return 100;
+        }
+        const diff = Math.random() * 5;
+        return Math.min(prevValue + diff, 100);
+      });
+    }, 300);
   };
 
-  const handleRouteChangeComplete = () => setValue(100);
+  const handleRouteChangeComplete = () => {
+    setValue(100);
+
+    if (progressingTimerRef.current) {
+      clearInterval(progressingTimerRef.current);
+    }
+
+    progressingDoneTimerRef.current = setTimeout(() => {
+      setIsDone(true);
+
+      valueInitializeTimerRef.current = setTimeout(() => setValue(0), 500);
+    }, 300);
+  };
 
   useEffect(() => {
     let pageProgress = document.getElementById('page-progress-root');
@@ -50,38 +83,6 @@ function PageProgress() {
 
     setIsMounted(true);
   }, []);
-
-  useEffect(() => {
-    if (value >= 100) {
-      if (progressingTimerRef.current) {
-        clearInterval(progressingTimerRef.current);
-      }
-
-      progressingDoneTimerRef.current = setTimeout(() => setIsDone(true), 300);
-    }
-  }, [value]);
-
-  useEffect(() => {
-    if (value === 0) {
-      if (progressingTimerRef.current) {
-        clearInterval(progressingTimerRef.current);
-      }
-
-      if (progressingDoneTimerRef.current) {
-        clearTimeout(progressingDoneTimerRef.current);
-      }
-
-      progressingTimerRef.current = setInterval(() => {
-        setValue((prevValue) => {
-          if (prevValue < 100) {
-            const diff = Math.random() * 5;
-            return Math.min(prevValue + diff, 100);
-          }
-          return 100;
-        });
-      }, 300);
-    }
-  }, [value]);
 
   useEffect(() => {
     router.events.on('routeChangeStart', handleRouteChangeStart);
@@ -108,6 +109,10 @@ function PageProgress() {
       if (progressingDoneTimerRef.current) {
         clearTimeout(progressingDoneTimerRef.current);
       }
+
+      if (valueInitializeTimerRef.current) {
+        clearTimeout(valueInitializeTimerRef.current);
+      }
     };
   }, []);
 
@@ -119,9 +124,8 @@ function PageProgress() {
           position: 'fixed',
           top: 0,
           left: 0,
-          width: '100%',
           opacity,
-          transition: 'opacity .5s ease-in',
+          transition: 'opacity .1s ease-in',
           zIndex: 10
         }}
       />,
