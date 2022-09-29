cbbc07b9f042271af09c504bcaf7c219f159d113
kimjh96 <kimjhs@kakao.com>
2022-09-29 22:18:53 +0900

    feat: 404, 500 페이지 theme-color 지정

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,30 +1,43 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-import { Box } from 'cocstorage-ui';
+import { Box, useTheme } from 'cocstorage-ui';
 
 import Message from '@components/UI/molecules/Message';
 
 function Custom404() {
   const router = useRouter();
 
+  const {
+    theme: {
+      palette: { background }
+    }
+  } = useTheme();
+
   const handleClose = () => router.push('/');
 
   return (
-    <Box
-      customStyle={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh'
-      }}
-    >
-      <Message
-        title="페이지를 찾을 수 없어요."
-        message="찾으시는 항목이 삭제되었거나,<br /> 잘못된 주소가 입력되었을 수 있어요."
-        buttonText="홈으로"
-        onClose={handleClose}
-      />
-    </Box>
+    <>
+      <Head>
+        <meta name="theme-color" content={background.bg} />
+      </Head>
+      <Box
+        component="main"
+        customStyle={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh'
+        }}
+      >
+        <Message
+          title="페이지를 찾을 수 없어요."
+          message="찾으시는 항목이 삭제되었거나,<br /> 잘못된 주소가 입력되었을 수 있어요."
+          buttonText="홈으로"
+          onClose={handleClose}
+        />
+      </Box>
+    </>
   );
 }
 
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,30 +1,43 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-import { Box } from 'cocstorage-ui';
+import { Box, useTheme } from 'cocstorage-ui';
 
 import Message from '@components/UI/molecules/Message';
 
 function Custom500() {
   const router = useRouter();
 
+  const {
+    theme: {
+      palette: { background }
+    }
+  } = useTheme();
+
   const handleClose = () => router.push('/');
 
   return (
-    <Box
-      customStyle={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh'
-      }}
-    >
-      <Message
-        title="알 수 없는 오류가 발생했어요."
-        message="요청을 처리하지 못했어요.<br />잠시 후에 다시 시도해 주세요!"
-        buttonText="홈으로"
-        onClose={handleClose}
-      />
-    </Box>
+    <>
+      <Head>
+        <meta name="theme-color" content={background.bg} />
+      </Head>
+      <Box
+        component="main"
+        customStyle={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh'
+        }}
+      >
+        <Message
+          title="알 수 없는 오류가 발생했어요."
+          message="요청을 처리하지 못했어요.<br />잠시 후에 다시 시도해 주세요!"
+          buttonText="홈으로"
+          onClose={handleClose}
+        />
+      </Box>
+    </>
   );
 }
 
