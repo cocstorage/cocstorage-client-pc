2402b02a8466a2861fb627399b28b422ddccb835
kimjh96 <kimjhs@kakao.com>
2023-02-12 08:41:00 +0900

    fix: 기타 수정

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-import { Box, useTheme } from 'cocstorage-ui';
+import { Flexbox, useTheme } from 'cocstorage-ui';
 
+import WideFlexibleTemplate from '@components/templeates/WideFlexibleTemplate';
 import Message from '@components/UI/molecules/Message';
 
 function Custom404() {
@@ -21,22 +22,22 @@ function Custom404() {
       <Head>
         <meta name="theme-color" content={background.bg} />
       </Head>
-      <Box
-        component="main"
-        customStyle={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100vh'
-        }}
-      >
-        <Message
-          title="페이지를 찾을 수 없어요."
-          message="찾으시는 항목이 삭제되었거나,<br /> 잘못된 주소가 입력되었을 수 있어요."
-          buttonText="홈으로"
-          onClose={handleClose}
-        />
-      </Box>
+      <WideFlexibleTemplate>
+        <Flexbox
+          alignment="center"
+          justifyContent="center"
+          customStyle={{
+            flex: 1
+          }}
+        >
+          <Message
+            title="페이지를 찾을 수 없어요."
+            message="찾으시는 항목이 삭제되었거나,<br /> 잘못된 주소가 입력되었을 수 있어요."
+            buttonText="홈으로"
+            onClose={handleClose}
+          />
+        </Flexbox>
+      </WideFlexibleTemplate>
     </>
   );
 }
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-import { Box, useTheme } from 'cocstorage-ui';
+import { Flexbox, useTheme } from 'cocstorage-ui';
 
+import WideFlexibleTemplate from '@components/templeates/WideFlexibleTemplate';
 import Message from '@components/UI/molecules/Message';
 
 function Custom500() {
@@ -21,22 +22,22 @@ function Custom500() {
       <Head>
         <meta name="theme-color" content={background.bg} />
       </Head>
-      <Box
-        component="main"
-        customStyle={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100vh'
-        }}
-      >
-        <Message
-          title="알 수 없는 오류가 발생했어요."
-          message="요청을 처리하지 못했어요.<br />잠시 후에 다시 시도해 주세요!"
-          buttonText="홈으로"
-          onClose={handleClose}
-        />
-      </Box>
+      <WideFlexibleTemplate>
+        <Flexbox
+          alignment="center"
+          justifyContent="center"
+          customStyle={{
+            flex: 1
+          }}
+        >
+          <Message
+            title="알 수 없는 오류가 발생했어요."
+            message="요청을 처리하지 못했어요.<br />잠시 후에 다시 시도해 주세요!"
+            buttonText="홈으로"
+            onClose={handleClose}
+          />
+        </Flexbox>
+      </WideFlexibleTemplate>
     </>
   );
 }
